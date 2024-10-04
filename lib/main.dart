96b34ff50397f7fc695afe56f8c56801f6f7a371
Athaboy <rafifi0607@gmail.com>
2024-10-04 09:37:03 +0700

    jkiijkmlk

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login App',
+      title: 'Login pppppp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
